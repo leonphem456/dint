512c6558776104aabfafde2ffef1ec463fbce03f
leonphem456 <leonphem@outlook.de>
2024-06-06 12:43:39 +0200

    added interfaces and test data

--- src/app/components/room-list/room-list.component.ts
+++ src/app/components/room-list/room-list.component.ts
@@ -2,22 +2,64 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule, MatAccordion } from '@angular/material/expansion';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
-interface Room{
-  name: string,
-  priority: string,
-  
+interface Room {
+  name: string;
+  description: string;
+  items: Item[];
+}
+
+interface Item {
+  name: string;
+  priority: PRIORITY;
+  img: string;
+}
+
+enum PRIORITY {
+  HIGH,
+  MEDIUM,
+  LOW,
 }
 
 @Component({
   selector: 'app-room-list',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatExpansionModule, MatAccordion],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatAccordion,
+    MatTooltipModule,
+  ],
   templateUrl: './room-list.component.html',
-  styleUrl: './room-list.component.scss'
+  styleUrl: './room-list.component.scss',
 })
 export class RoomListComponent {
-  rooms = ["Wohnzimmer", "Schlafzimmer", "Badezimmer", "Keller"]
-
+  rooms: Room[] = [
+    {
+      name: 'Wohnzimmer',
+      description: 'Wohnraum',
+      items: [{ name: 'TV', priority: PRIORITY.HIGH, img: '' }],
+    },
+    {
+      name: 'Schlafzimmer',
+      description: '',
+      items: [{ name: 'Bett', priority: PRIORITY.HIGH, img: '' }],
+    },
+    {
+      name: 'Badezimmer',
+      description: '',
+      items: [
+        { name: 'Bürste', priority: PRIORITY.HIGH, img: '' },
+        { name: 'Dior Perfume', priority: PRIORITY.LOW, img: '' },
+      ],
+    },
+    {
+      name: 'Keller',
+      description: '',
+      items: [{ name: 'Alte Magazine', priority: PRIORITY.LOW, img: '' }],
+    },
+  ];
   panelOpenState = false;
-};
+}

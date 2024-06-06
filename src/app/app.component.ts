3734df634bd3603c10a99fb63488f6388786a777
leonphem456 <leonphem@outlook.de>
2024-06-06 12:44:19 +0200

    reformatted code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MenubarComponent } from './components/menubar/menubar.component';
 import { RoomListComponent } from './components/room-list/room-list.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MenubarComponent, RoomListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dint';

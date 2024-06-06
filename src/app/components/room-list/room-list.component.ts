b3725c58c2e6e7e56d1b1ef191e7984f104808db
leonphem456 <leonphem@outlook.de>
2024-06-07 01:10:15 +0200

    added card component for items

--- src/app/components/room-list/room-list.component.ts
+++ src/app/components/room-list/room-list.component.ts
@@ -3,6 +3,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule, MatAccordion } from '@angular/material/expansion';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 interface Room {
   name: string;
@@ -14,12 +16,13 @@ interface Item {
   name: string;
   priority: PRIORITY;
   img: string;
+  note: string;
 }
 
 enum PRIORITY {
-  HIGH,
-  MEDIUM,
-  LOW,
+  HIGH = 'HIGH',
+  MEDIUM = 'MEDIUM',
+  LOW = 'LOW',
 }
 
 @Component({
@@ -31,6 +34,8 @@ enum PRIORITY {
     MatExpansionModule,
     MatAccordion,
     MatTooltipModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   templateUrl: './room-list.component.html',
   styleUrl: './room-list.component.scss',
@@ -40,25 +45,56 @@ export class RoomListComponent {
     {
       name: 'Wohnzimmer',
       description: 'Wohnraum',
-      items: [{ name: 'TV', priority: PRIORITY.HIGH, img: '' }],
+      items: [
+        {
+          name: 'TV',
+          priority: PRIORITY.HIGH,
+          img: 'assets/tv.png',
+          note: 'Tv for watching stuff',
+        },
+      ],
     },
     {
       name: 'Schlafzimmer',
       description: '',
-      items: [{ name: 'Bett', priority: PRIORITY.HIGH, img: '' }],
+      items: [
+        {
+          name: 'Bett',
+          priority: PRIORITY.HIGH,
+          img: '',
+          note: '',
+        },
+      ],
     },
     {
       name: 'Badezimmer',
       description: '',
       items: [
-        { name: 'Bürste', priority: PRIORITY.HIGH, img: '' },
-        { name: 'Dior Perfume', priority: PRIORITY.LOW, img: '' },
+        {
+          name: 'Bürste',
+          priority: PRIORITY.HIGH,
+          img: '',
+          note: 'new',
+        },
+        {
+          name: 'Dior Perfume',
+          priority: PRIORITY.LOW,
+          img: '',
+          note: 'Almost used up',
+        },
       ],
     },
     {
       name: 'Keller',
       description: '',
-      items: [{ name: 'Alte Magazine', priority: PRIORITY.LOW, img: '' }],
+      items: [
+        {
+          name: 'Alte Magazine',
+          priority: PRIORITY.LOW,
+          img: '',
+          note: 'Haushalt und Mode',
+        },
+      ],
     },
   ];
   panelOpenState = false;
